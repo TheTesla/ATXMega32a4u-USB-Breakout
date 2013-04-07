edc6de677c5b08ab2bdf8a8ef19a1d1d04e83e60
TheTesla <stefan.helmert@gmx.net>
2013-04-07 05:30:06 +0200

    Update BOM, Rename-Script
    Signed-off-by: TheTesla <stefan.helmert@gmx.net>

---
+++ rename.sh
@@ -0,0 +1,17 @@
+proname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="pro" { print $1;}')
+gtlname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gtl" { print $1;}')
+gblname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gbl" { print $1;}')
+gtsname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gts" { print $1;}')
+gbsname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gbs" { print $1;}')
+gbrname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gbr" { print $1;}')
+gtoname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gto" { print $1;}')
+gboname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gbo" { print $1;}')
+drlname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="drl" { print $1;}')
+mv -f $gtlname.gtl $proname.GTL
+mv -f $gblname.gbl $proname.GBL
+mv -f $gtsname.gts $proname.GTS
+mv -f $gbsname.gbs $proname.GBS
+mv -f $gbrname.gbr $proname.GBR
+mv -f $gtoname.gto $proname.GTO
+mv -f $gboname.gbo $proname.GBO
+mv -f $drlname.drl $proname.XLN

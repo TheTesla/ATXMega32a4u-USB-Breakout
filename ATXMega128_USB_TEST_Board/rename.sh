daf57b853940f7d13aee1da034f560098ecae91e
TheTesla <stefan.helmert@gmx.net>
2013-09-13 22:34:14 +0200

    Power Supply Diode added, Board Edge Cuts added
    Signed-off-by: TheTesla <stefan.helmert@gmx.net>

---
+++ ATXMega128_USB_TEST_Board/rename.sh
@@ -0,0 +1,26 @@
+proname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="pro" { print $1;}')
+gtlname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gtl" { print $1;}')
+gblname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gbl" { print $1;}')
+gtsname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gts" { print $1;}')
+gbsname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gbs" { print $1;}')
+gbrname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gbr" { print $1;}')
+gtoname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gto" { print $1;}')
+gboname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="gbo" { print $1;}')
+drlname=$(echo * | awk 'BEGIN {RS=" "; FS="."}; $2=="drl" { print $1;}')
+mv -f $gtlname.gtl $proname.GTL
+mv -f $gblname.gbl $proname.GBL
+mv -f $gtsname.gts $proname.GTS
+mv -f $gbsname.gbs $proname.GBS
+mv -f $gbrname.gbr $proname.GKO
+mv -f $gtoname.gto $proname.GTO
+mv -f $gboname.gbo $proname.GBO
+mv -f $drlname.drl $proname.XLN
+7z a $proname.ZIP $proname.GTL $proname.GBL $proname.GTS $proname.GBS $proname.GKO $proname.GTO $proname.GBO $proname.XLN
+#rm $proname.GTL
+#rm $proname.GBL
+#rm $proname.GTS
+#rm $proname.GBS
+#rm $proname.GKO
+#rm $proname.GTO
+#rm $proname.GBO
+#rm $proname.XLN

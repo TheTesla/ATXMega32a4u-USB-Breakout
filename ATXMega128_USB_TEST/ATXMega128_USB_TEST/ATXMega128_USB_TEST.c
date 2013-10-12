2cee8da8db71dd1a9afed6d6d87ec1a07c30e928
TheTesla <stefan.helmert@gmx.net>
2013-10-12 21:13:19 +0200

    testboard images and DFU USB bootloader added

--- ATXMega128_USB_TEST/ATXMega128_USB_TEST/ATXMega128_USB_TEST.c
+++ ATXMega128_USB_TEST/ATXMega128_USB_TEST/ATXMega128_USB_TEST.c
@@ -5,10 +5,8 @@
  *  Author: Stefan
  */ 
 
-#define F_CPU 30000000UL
 
 #include <avr/io.h>
-#include <util/delay.h> 
 
 
 
@@ -33,8 +31,6 @@ int main(void)
 	i = x = y = z = 0;
 	j = ok = ct = 0;
 	
-	//PORTA.OUT = 4;
-	//while(1);
 	while(1){
 		PORTA.OUT = (PORTA.OUT & 0b11111011) | ((~PORTA.IN & 0b00000001) << 2);
 		PORTB.OUT = (PORTB.OUT & 0b11111011) | ((~PORTB.IN & 0b00000001) << 2);
@@ -173,7 +169,6 @@ int main(void)
 			if(ct < 1){
 				PORTE.DIR = 0b00000001; // damit Quarz nicht über testpins belastet wird
 
-
 				// Enable the external oscillator
 				OSC.XOSCCTRL = OSC_FRQRANGE_2TO9_gc | OSC_XOSCSEL_XTAL_16KCLK_gc ;
 				OSC.CTRL |= OSC_XOSCEN_bm ;
@@ -194,138 +189,13 @@ int main(void)
 				CCP = CCP_IOREG_gc; // protected write follows
 				CLK.CTRL = CLK_SCLKSEL_RC2M_gc;
 
-
 				PORTE.DIR = 0b00001011; // PR0 und PR1 wieder verfuegbar
 				// Disable the external oscillator
 				OSC.CTRL &= ~OSC_XOSCEN_bm;
 				OSC.CTRL &= ~OSC_PLLEN_bm ; // disable the PLL...
 
-				
-				
 			}
-			
 		}
-		
-		
-		
-		
-		
-		
-		
-		
 	}
 
-
-
-	x = 0;
-	PORTD.DIR = 0xFF;
-	PORTE.DIR = 0xFF;
-	
-	PORTC.DIR = 0xE0;
-
-
-	
-	PORTC.OUTCLR = 1 << 6;
-	PORTC.OUTSET = 1 << 7;
-	
-	// Enable the external oscillator
-	OSC.XOSCCTRL = OSC_FRQRANGE_2TO9_gc | OSC_XOSCSEL_XTAL_16KCLK_gc ;
-	OSC.CTRL |= OSC_XOSCEN_bm ;
-	while( (OSC.STATUS & OSC_XOSCRDY_bm) == 0 ){} // wait until stable
-
-	// Now configure the PLL to be eXternal OSCillator * 2
-	OSC.PLLCTRL = OSC_PLLSRC_XOSC_gc | 3 ;
-	OSC.CTRL |= OSC_PLLEN_bm ; // enable the PLL...
-	while( (OSC.STATUS & OSC_PLLRDY_bm) == 0 ){} // wait until stable
-
-	// And now switch to the PLL as the clocksource
-	CCP = CCP_IOREG_gc; // protected write follows
-	CLK.CTRL = CLK_SCLKSEL_PLL_gc;
-	
-	
-
-
-		
-    while(1)
-    {
-		x = 0;
-		PORTD.OUT = 0;
-		PORTE.OUT = 0;
-		PORTA.OUT = 0;
-		PORTC.OUTCLR = 1 << 5;
-
-
-		_delay_ms(50);
-		for(i=0;i<14;i++){
-			if(8==i) continue;
-			y = 1 << i;
-			if(12>i){
-				PORTD.OUT = y & 0xFF;
-				PORTE.OUT = (y >> 8) & 0x0F;
-				PORTA.OUT = 0;
-				PORTC.OUTCLR = 1 << 5;
-			}
-			else if(12==i) {
-				PORTD.OUT = 0;
-				PORTE.OUT = 0;
-				PORTA.OUT = 2;
-				PORTC.OUTCLR = 1 << 5;
-			}
-			else if(13==i){
-				PORTD.OUT = 0;
-				PORTE.OUT = 0;
-				PORTA.OUT = 16;
-				PORTC.OUTCLR = 1 << 5;
-			}
-			z = (((uint32_t)PORTC.IN & 0x001F) << 12) + (((uint32_t)PORTB.IN & 0x000F) << 8) + ((uint32_t)PORTA.IN & 0x00EC);
-			if(12>i){
-				if(((uint32_t)1<<((uint32_t)16-i)) != z) x += y;			
-			}
-			else if(12==i){
-				if(8!=z) x += y;
-			}
-			else if(13==i){
-				if(4!=z) x += y;
-			}
-		}
-		
-		PORTD.OUT = 0;
-		PORTE.OUT = 0;
-		PORTA.OUT = 0;
-		PORTC.OUTCLR = 1 << 5;
-
-		_delay_ms(50);
-		PORTE.OUT = 1;
-		_delay_ms(10);
-		if((0 != PORTB.IN) || (0 != PORTA.IN)) x += 1 << 13;
-		_delay_ms(50);
-		if((1 != PORTB.IN) || (0 != PORTA.IN)) x += 1 << 14;
-
-
-		PORTD.OUT = 0;
-		PORTE.OUT = 0;
-		PORTA.OUT = 0;
-		PORTC.OUTCLR = 1 << 5;
-		_delay_ms(50);
-		PORTC.OUTSET = 1 << 5;
-		_delay_ms(10);
-		if((0 != PORTB.IN) || (0 != PORTA.IN)) x += 1 << 13;
-		_delay_ms(50);
-		if((0 != PORTB.IN) || (1 != PORTA.IN)) x += 1 << 14;
-		
-	
-		if(0==x) {
-			PORTC.OUTTGL = 1 << 6;
-			PORTC.OUTCLR = 1 << 7;
-		}		
-		else{
-			PORTC.OUTCLR = 1 << 6;
-			PORTC.OUTTGL = 1 << 7;
-		}	
-		
-		
-
-		
-        //TODO:: Please write your application code 
-    }
 }
--- ATXMega128_USB_TEST/ATXMega128_USB_TEST/Debug/ATXMega128_USB_TEST.d
+++ ATXMega128_USB_TEST/ATXMega128_USB_TEST/Debug/ATXMega128_USB_TEST.d
@@ -9,10 +9,7 @@ ATXMega128_USB_TEST.d ATXMega128_USB_TEST.o: .././ATXMega128_USB_TEST.c \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/common.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/version.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/fuse.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay_basic.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/math.h
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/io.h:
 
@@ -35,9 +32,3 @@ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/fuse.h:
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h:
-
-c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay.h:
-
-c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay_basic.h:
-
-c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.939\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/math.h:
